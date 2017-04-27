0c80c8300fc90c6fed5ce4d7ec6c409d044747a4
Xi Xiao <xx565@nyu.edu>
2017-04-27 02:33:42 -0400

    pass all tests

--- vagrant/tournament/tournament.py
+++ vagrant/tournament/tournament.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
-# 
+#
 # tournament.py -- implementation of a Swiss-system tournament
 #
 
-import psycopg2
+import psycopg2, bleach
 
+DBNAME = "tournament"
 
 def connect():
     """Connect to the PostgreSQL database.  Returns a database connection."""
@@ -13,26 +14,45 @@ def connect():
 
 def deleteMatches():
     """Remove all the match records from the database."""
-
+    db = psycopg2.connect(database=DBNAME)
+    c = db.cursor()
+    c.execute("delete from matches")
+    db.commit()
+    db.close()
 
 def deletePlayers():
     """Remove all the player records from the database."""
+    db = psycopg2.connect(database=DBNAME)
+    c = db.cursor()
+    c.execute("delete from players")
+    db.commit()
+    db.close()
 
 
 def countPlayers():
     """Returns the number of players currently registered."""
+    db = psycopg2.connect(database=DBNAME)
+    c = db.cursor()
+    c.execute("select * from players")
+    players = c.fetchall()
+    db.close()
+    return len(players)
 
 
 def registerPlayer(name):
     """Adds a player to the tournament database.
-  
+
     The database assigns a unique serial id number for the player.  (This
     should be handled by your SQL database schema, not in your Python code.)
-  
+
     Args:
       name: the player's full name (need not be unique).
     """
-
+    db = psycopg2.connect(database=DBNAME)
+    c = db.cursor()
+    c.execute("insert into players values (%s)", (bleach.clean(name),))
+    db.commit()
+    db.close()
 
 def playerStandings():
     """Returns a list of the players and their win records, sorted by wins.
@@ -47,7 +67,12 @@ def playerStandings():
         wins: the number of matches the player has won
         matches: the number of matches the player has played
     """
-
+    db = psycopg2.connect(database=DBNAME)
+    c = db.cursor()
+    c.execute("select * from playerstandings")
+    playerstandings = c.fetchall()
+    db.close()
+    return playerstandings
 
 def reportMatch(winner, loser):
     """Records the outcome of a single match between two players.
@@ -56,16 +81,20 @@ def reportMatch(winner, loser):
       winner:  the id number of the player who won
       loser:  the id number of the player who lost
     """
- 
- 
+    db = psycopg2.connect(database=DBNAME)
+    c = db.cursor()
+    c.execute("insert into matches values (%s,%s)", ((bleach.clean(winner),), (bleach.clean(loser),)))
+    db.commit()
+    db.close()
+
 def swissPairings():
     """Returns a list of pairs of players for the next round of a match.
-  
+
     Assuming that there are an even number of players registered, each player
     appears exactly once in the pairings.  Each player is paired with another
     player with an equal or nearly-equal win record, that is, a player adjacent
     to him or her in the standings.
-  
+
     Returns:
       A list of tuples, each of which contains (id1, name1, id2, name2)
         id1: the first player's unique id
@@ -73,5 +102,17 @@ def swissPairings():
         id2: the second player's unique id
         name2: the second player's name
     """
-
+    db = psycopg2.connect(database=DBNAME)
+    c = db.cursor()
+    c.execute("select id, name from playerstandings")
+    idandnamepairs = c.fetchall()
+    db.close()
+    r = []
+    index = 0;
+    while index < len(idandnamepairs):
+        (id1, name1) = idandnamepairs[index]
+        (id2, name2) = idandnamepairs[index + 1]
+        r.append((id1, name1, id2, name2))
+        index += 2
+    return r
 
--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -6,4 +6,35 @@
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
 
+-- DROP DATABASE IF EXISTS tournament;
+-- CREATE DATABASE tournament;
 
+\c tournament;
+DROP VIEW IF EXISTS playerstandings;
+DROP VIEW IF EXISTS wins;
+DROP VIEW IF EXISTS total;
+DROP TABLE IF EXISTS players;
+DROP TABLE IF EXISTS matches;
+
+CREATE TABLE players ( name TEXT,
+                     id SERIAL);
+CREATE TABLE matches ( winner integer,
+                     loser integer);
+CREATE VIEW wins AS
+SELECT players.id, COUNT(*) AS num
+FROM players, matches
+WHERE players.id = matches.winner
+GROUP BY players.id;
+
+CREATE VIEW total AS
+SELECT players.id, COUNT(*) AS num
+FROM players, matches
+WHERE players.id = matches.winner OR players.id = matches.loser
+GROUP BY players.id;
+
+CREATE VIEW playerstandings AS
+SELECT players.id, players.name, coalesce(wins.num, 0) AS winnum, coalesce(total.num, 0) AS totalnum
+FROM players
+LEFT JOIN wins USING (id)
+LEFT JOIN total USING (id)
+ORDER BY wins.num DESC;
